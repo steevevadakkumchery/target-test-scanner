f4f898aca72e98171155c62990e274f5c5b58ef0
Steeve Vadakkumchery <steeve.vadakkumchery@vodafone.com>
2022-01-24 11:49:05 +0000

    fixed port and error management

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const { JSDOM } = require('jsdom')
-const PORT = 8001
+
+const PORT = process.env.PORT || 8001
 // const url = 'https://www.vodafone.co.uk/mobile/phones/pay-monthly-contracts/samsung/galaxy-s21-fe'
 // const url = 'https://www.vodafone.co.uk/business/business-sim-only?commitmentPeriod=24%20Months&icmp=uk~1_consumer~topnav~1_shop~1_sim_only_&_plans~1_sim_only_for_phones&linkpos=topnav~1~1~1~1'
 const basePath = 'https://www.vodafone.co.uk/'
 const app = express()
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   console.log('Request Id:', req.query.href);
   const data = await getTestInfo(`${basePath}${req.query.href}`)
-  const processedData = data.reduce((acc, curr) => {
-    return [ ...acc, {testName: curr.CampaignName} ]
-  }, [])
-  res.json(processedData)
+  if(Array.isArray(data)) {
+    const processedData = data.reduce((acc, curr) => {
+      return [ ...acc, {testName: curr.CampaignName} ]
+    }, [])
+    res.status(200)
+    res.json(processedData)
+    res.end()
+  }
+  res.status(408)
+  res.write('request timed out')
+  res.end()
 })
 
 app.listen(PORT, () => {
